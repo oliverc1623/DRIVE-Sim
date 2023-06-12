e78c0630adf8ed250613b74c249f7e5daa550e44
Oliver Chang <oliverc1622@gmail.com>
2023-06-11 22:49:27 -0700

    started adapting to vista

---
+++ minimal/vista_a2c.py
@@ -0,0 +1,247 @@
+import gymnasium as gym
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Categorical
+import torch.multiprocessing as mp
+import numpy as np
+import vista
+import os
+
+# Hyperparameters
+n_train_processes = 3
+learning_rate = 0.0002
+update_interval = 5
+gamma = 0.98
+max_train_steps = 60000
+PRINT_INTERVAL = update_interval * 100
+
+class ActorCritic(nn.Module):
+    def __init__(self):
+        super(ActorCritic, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.norm1 = nn.GroupNorm(8, 32)
+        self.relu1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.norm2 = nn.GroupNorm(16, 64)
+        self.relu2 = nn.ReLU()
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.norm3 = nn.GroupNorm(32, 128)
+        self.relu3 = nn.ReLU()
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.norm4 = nn.GroupNorm(64, 256)
+        self.relu4 = nn.ReLU()
+        self.conv5 = nn.Conv2d(256, 2, kernel_size=3, stride=1, padding=1)
+        self.norm5 = nn.GroupNorm(1, 2)
+        self.relu5 = nn.ReLU()
+        self.fc = nn.Linear(2 * 32 * 30, 2)
+        self.fc_v = nn.Linear(3 * 32 * 20, 1)
+
+    def pi(self, x):
+        single_image_input = len(x.shape) == 3  # missing 4th batch dimension
+        if single_image_input:
+            x = x.unsqueeze(0)
+            x = x.permute(0, 3, 1, 2)
+        else:
+            x = x.view(x.shape[0] * x.shape[1], 3, 30, 32)
+        x = self.relu1(self.norm1(self.conv1(x)))
+        x = self.relu2(self.norm2(self.conv2(x)))
+        x = self.relu3(self.norm3(self.conv3(x)))
+        x = self.relu4(self.norm4(self.conv4(x)))
+        x = self.relu5(self.norm5(self.conv5(x)))
+        x = x.reshape(x.size(0), -1)
+        x = self.fc(x)
+
+        mu, log_sigma = torch.chunk(x, 2, dim=-1)
+        mu = 1/8.0 * torch.tanh(mu)  # conversion
+        sigma = 0.1 * torch.sigmoid(log_sigma) + 0.005  # conversion
+        return mu, sigma
+    
+    def v(self, x):
+        x = x.view(x.shape[0]*x.shape[1], 3, 30, 32)
+        x = self.relu1(self.norm1(self.conv1(x)))
+        x = self.relu2(self.norm2(self.conv2(x)))
+        x = self.relu3(self.norm3(self.conv3(x)))
+        x = self.relu4(self.norm4(self.conv4(x)))
+        x = self.relu5(self.norm5(self.conv5(x)))
+        x = x.reshape(x.size(0), -1)
+        v = self.fc_v(x)
+        return v
+
+def worker(worker_id, master_end, worker_end):
+    master_end.close()  # Forbid worker to use the master end for messaging
+    trace_root = "../trace"
+    trace_path = [
+        "20210726-154641_lexus_devens_center",
+        "20210726-155941_lexus_devens_center_reverse",
+        "20210726-184624_lexus_devens_center",
+        "20210726-184956_lexus_devens_center_reverse",
+    ]
+    trace_path = [os.path.join(trace_root, p) for p in trace_path]
+    world = vista.World(trace_path, trace_config={"road_width": 4})
+    car = world.spawn_agent(
+        config={
+            "length": 5.0,
+            "width": 2.0,
+            "wheel_base": 2.78,
+            "steering_ratio": 14.7,
+            "lookahead_road": True,
+        }
+    )
+    camera = car.spawn_camera(config={"size": (200, 320)})
+    display = vista.Display(
+        world, display_config={"gui_scale": 2, "vis_full_frame": False}
+    )
+
+    while True:
+        cmd, data = worker_end.recv()
+        if cmd == 'step':
+            #ob, reward, done, info = env.step(data)
+            ob, reward, done, truncated, info = env.step(data)
+            if done:
+                ob, info = env.reset()
+            worker_end.send((ob, reward, done, info))
+        elif cmd == 'reset':
+            ob, info = env.reset()
+            worker_end.send(ob)
+        elif cmd == 'reset_task':
+            ob = env.reset_task()
+            worker_end.send(ob)
+        elif cmd == 'close':
+            worker_end.close()
+            break
+        elif cmd == 'get_spaces':
+            worker_end.send((env.observation_space, env.action_space))
+        else:
+            raise NotImplementedError
+
+class ParallelEnv:
+    def __init__(self, n_train_processes):
+        self.nenvs = n_train_processes
+        self.waiting = False
+        self.closed = False
+        self.workers = list()
+
+        master_ends, worker_ends = zip(*[mp.Pipe() for _ in range(self.nenvs)])
+        self.master_ends, self.worker_ends = master_ends, worker_ends
+
+        for worker_id, (master_end, worker_end) in enumerate(zip(master_ends, worker_ends)):
+            p = mp.Process(target=worker,
+                           args=(worker_id, master_end, worker_end))
+            p.daemon = True
+            p.start()
+            self.workers.append(p)
+
+        # Forbid master to use the worker end for messaging
+        for worker_end in worker_ends:
+            worker_end.close()
+
+    def step_async(self, actions):
+        for master_end, action in zip(self.master_ends, actions):
+            master_end.send(('step', action))
+        self.waiting = True
+
+    def step_wait(self):
+        results = [master_end.recv() for master_end in self.master_ends]
+        self.waiting = False
+        obs, rews, dones, infos = zip(*results)
+        return np.stack(obs), np.stack(rews), np.stack(dones), np.stack(infos)
+
+    def reset(self):
+        for master_end in self.master_ends:
+            master_end.send(('reset', None))
+        return np.stack([master_end.recv() for master_end in self.master_ends])
+
+    def step(self, actions):
+        self.step_async(actions)
+        return self.step_wait()
+
+    def close(self):  # For clean up resources
+        if self.closed:
+            return
+        if self.waiting:
+            [master_end.recv() for master_end in self.master_ends]
+        for master_end in self.master_ends:
+            master_end.send(('close', None))
+        for worker in self.workers:
+            worker.join()
+            self.closed = True
+
+def test(step_idx, model, device):
+    env = gym.make('CartPole-v1', render_mode = 'rgb_array')
+    score = 0.0
+    num_test = 10
+
+    for _ in range(num_test):
+        observation, info = env.reset()
+        terminated = False
+        truncated = False
+        while not terminated and not truncated:
+            prob = model.pi(torch.from_numpy(observation).float().to(device), softmax_dim=0)
+            action = Categorical(prob).sample().cpu().numpy()
+            observation_prime, reward, terminated, truncated, info = env.step(action)
+            observation = observation_prime
+            score += reward
+        terminated = False
+    print(f"Step # :{step_idx}, avg score : {score/num_test:.1f}")
+
+    env.close()
+
+def compute_target(v_final, r_lst, mask_lst):
+    G = v_final.reshape(-1)
+    td_target = list()
+
+    for r, mask in zip(r_lst[::-1], mask_lst[::-1]):
+        G = r + gamma * G * mask
+        td_target.append(G)
+
+    return torch.tensor(td_target[::-1]).float()
+
+if __name__ == '__main__':
+    device = 'cpu'
+    print('device:{}'.format(device))
+    envs = ParallelEnv(n_train_processes)
+
+    model = ActorCritic().to(device)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    step_idx = 0
+    s = envs.reset()
+    while step_idx < max_train_steps:
+        s_lst, a_lst, r_lst, mask_lst = list(), list(), list(), list()
+        for _ in range(update_interval):
+            prob = model.pi(torch.from_numpy(s).float().to(device))
+            a = Categorical(prob).sample().cpu().numpy()
+            s_prime, r, done, info = envs.step(a)
+
+            s_lst.append(s)
+            a_lst.append(a)
+            r_lst.append(r/100.0)
+            mask_lst.append(1 - done)
+
+            s = s_prime
+            step_idx += 1
+
+        s_final = torch.from_numpy(s_prime).float().to(device)
+        v_final = model.v(s_final).detach().cpu().clone().numpy()
+        td_target = compute_target(v_final, r_lst, mask_lst)
+
+        td_target_vec = td_target.reshape(-1)
+        s_vec = torch.tensor(s_lst).float().reshape(-1, 4).to(device)  # 4 == Dimension of state
+        a_vec = torch.tensor(a_lst).reshape(-1).unsqueeze(1).to(device)
+        advantage = td_target_vec.to(device) - model.v(s_vec).reshape(-1)
+
+        pi = model.pi(s_vec, softmax_dim=1)
+        pi_a = pi.gather(1, a_vec).reshape(-1)
+        loss = -(torch.log(pi_a).to(device) * advantage.detach()).mean() +\
+            F.smooth_l1_loss(model.v(s_vec).reshape(-1).to(device), td_target_vec.to(device))
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if step_idx % PRINT_INTERVAL == 0:
+            test(step_idx, model, device)
+
+    envs.close()
