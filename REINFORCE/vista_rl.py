96c24e9b67fb320105ff88f6436eed55282c33bd
Oliver Chang <oliverc1622@gmail.com>
2023-06-28 11:19:19 -0700

    added filename arg

--- REINFORCE/vista_rl.py
+++ REINFORCE/vista_rl.py
@@ -14,22 +14,24 @@ def main(args):
     print(args.neuralnetwork)
     print(f"Animate: {args.animate}")
     learner = Learner(args.neuralnetwork, 
-                      args.learning_rate, 
-                      args.episodes, 
-                      args.clip, 
-                      args.animate,
-                      args.algorithm)
+                    args.learning_rate, 
+                    args.episodes, 
+                    args.clip, 
+                    args.animate,
+                    args.algorithm,
+                    args.filename)
     learner.learn()
-    # learner.save()
+    learner.save()
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description="VISTA Deep Reinforcement Learner")
-    parser.add_argument("-nn", "--neuralnetwork", required=True)
+    parser.add_argument("-nn", "--neuralnetwork")
     parser.add_argument("-lr", "--learning_rate", default=1e-4, type=float)
     parser.add_argument("-e", "--episodes", default=500, type=int)
     parser.add_argument("-c", "--clip", default=2, type=int)
     parser.add_argument("-a", "--animate", default=False, action='store_true', help='Bool type')
     parser.add_argument("--algorithm", default='vpg')
+    parser.add_argument("-f", "--filename")
     args = parser.parse_args()
     main(args)
 
