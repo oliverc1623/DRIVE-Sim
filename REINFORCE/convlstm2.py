a5ea8b163c05088160691fab6d37c3c9b10f6c9e
Oliver Chang <oliverc1622@gmail.com>
2023-07-18 14:08:01 -0700

    tried replacing resnet with 5-layer cnn

--- REINFORCE/convlstm2.py
+++ REINFORCE/convlstm2.py
@@ -9,8 +9,22 @@ from torchvision.models import resnet18, resnet101
 class CNNLSTM(nn.Module):
     def __init__(self, num_classes=2):
         super(CNNLSTM, self).__init__()
-        self.resnet = resnet18(pretrained=True)
-        self.resnet.fc = nn.Sequential(nn.Linear(self.resnet.fc.in_features, 300))
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.norm1 = nn.GroupNorm(8, 32)
+        self.relu1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.norm2 = nn.GroupNorm(16, 64)
+        self.relu2 = nn.ReLU()
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.norm3 = nn.GroupNorm(32, 128)
+        self.relu3 = nn.ReLU()
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.norm4 = nn.GroupNorm(64, 256)
+        self.relu4 = nn.ReLU()
+        self.conv5 = nn.Conv2d(256, 2, kernel_size=3, stride=1, padding=1)
+        self.norm5 = nn.GroupNorm(1, 2)
+        self.relu5 = nn.ReLU()
+        self.fc = nn.Linear(2 * 32 * 30, 300)
         self.lstm = nn.LSTM(input_size=300, hidden_size=256, num_layers=3)
         self.fc1 = nn.Linear(256, 128)
         self.fc2 = nn.Linear(128, num_classes)
@@ -19,7 +33,14 @@ class CNNLSTM(nn.Module):
         hidden = None
         for t in range(x.size(1)):
             with torch.no_grad():
-                x = self.resnet(x[:, t, :, :, :])
+                x = x[:, t, :, :, :]
+                x = self.relu1(self.norm1(self.conv1(x)))
+                x = self.relu2(self.norm2(self.conv2(x)))
+                x = self.relu3(self.norm3(self.conv3(x)))
+                x = self.relu4(self.norm4(self.conv4(x)))
+                x = self.relu5(self.norm5(self.conv5(x)))
+                x = x.reshape(x.size(0), -1)
+                x = self.fc(x)
             out, hidden = self.lstm(x.unsqueeze(0), hidden)
         x = self.fc1(out[-1, :, :])
         x = F.relu(x)
