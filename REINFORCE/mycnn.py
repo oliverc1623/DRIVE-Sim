f175716b602175dd5da6646516a2bb2e03219581
Oliver Chang <oliverc1622@gmail.com>
2023-07-18 14:14:15 -0700

    condensed code

--- REINFORCE/mycnn.py
+++ REINFORCE/mycnn.py
@@ -8,46 +8,26 @@ class CNN(nn.Module):
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
         self.norm1 = nn.GroupNorm(8, 32)
         self.relu1 = nn.ReLU()
-
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
         self.norm2 = nn.GroupNorm(16, 64)
         self.relu2 = nn.ReLU()
-
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
         self.norm3 = nn.GroupNorm(32, 128)
         self.relu3 = nn.ReLU()
-
         self.conv4 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
         self.norm4 = nn.GroupNorm(64, 256)
         self.relu4 = nn.ReLU()
-
         self.conv5 = nn.Conv2d(256, 2, kernel_size=3, stride=1, padding=1)
         self.norm5 = nn.GroupNorm(1, 2)
         self.relu5 = nn.ReLU()
-        
         self.fc = nn.Linear(2 * 32 * 30, 2)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.norm1(x)
-        x = self.relu1(x)
-
-        x = self.conv2(x)
-        x = self.norm2(x)
-        x = self.relu2(x)
-
-        x = self.conv3(x)
-        x = self.norm3(x)
-        x = self.relu3(x)
-
-        x = self.conv4(x)
-        x = self.norm4(x)
-        x = self.relu4(x)
-
-        x = self.conv5(x)
-        x = self.norm5(x)
-        x = self.relu5(x)
-
+        x = self.relu1(self.norm1(self.conv1(x)))
+        x = self.relu2(self.norm2(self.conv2(x)))
+        x = self.relu3(self.norm3(self.conv3(x)))
+        x = self.relu4(self.norm4(self.conv4(x)))
+        x = self.relu5(self.norm5(self.conv5(x)))
         x = x.reshape(x.size(0), -1)
         x = self.fc(x)
         mu, log_sigma = torch.chunk(x, 2, dim=-1)
