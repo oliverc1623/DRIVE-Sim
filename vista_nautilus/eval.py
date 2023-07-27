5c2320e4ebed01030daae6467b2124a3894f5a8b
oliverc1623 <oliverc1622@gmail.com>
2023-07-27 16:34:57 +0000

    added file args to eval

--- vista_nautilus/eval.py
+++ vista_nautilus/eval.py
@@ -5,8 +5,6 @@ import torch
 from torch.distributions import Normal
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
 import sys
 # caution: path[0] is reserved for script path (or '' in REPL)
 sys.path.insert(1, '../vista_nautilus/models/')
@@ -16,47 +14,14 @@ import a2c_cnn
 import vista
 from helper import *
 import datetime
+import argparse
+
 
 device = ("cuda" if torch.cuda.is_available() else "cpu")
 device = torch.device(device)
 print(f"Using {device} device")
 
-# REINFORCE
-reinforce_model = mycnn.CNN().to(device)
-reinforce_cnn_dict = torch.load('saved_models/REINFORCE_CNN_model.pth')
-reinforce_model.load_state_dict(reinforce_cnn_dict['model_state_dict'])
-reinforce_model.eval()
-
-# A2C
-a2c_model = a2c_cnn.ActorCritic().to(device)
-a2c_cnn_dict = torch.load('saved_models/best_a2c_model_checkpoint.pth')
-a2c_model.load_state_dict(a2c_cnn_dict['model_state_dict'])
-a2c_model.eval()
-
-# TODO: PPO
-
-# Start VISTA Environment
-trace_root = "vista_traces"
-trace_path = [
-    "20210726-154641_lexus_devens_center",
-    "20210726-155941_lexus_devens_center_reverse",
-    "20210726-184624_lexus_devens_center",
-    "20210726-184956_lexus_devens_center_reverse",
-]
-trace_path = [os.path.join(trace_root, p) for p in trace_path]
-world = vista.World(trace_path, trace_config={"road_width": 4})
-car = world.spawn_agent(config={"length": 5.0,
-                                "width": 2.0,
-                                "wheel_base": 2.78,
-                                "steering_ratio": 14.7,
-                                "lookahead_road": True,})
-camera = car.spawn_camera(config={"size": (355, 413)})
-display = vista.Display(
-    world, display_config={"gui_scale": 2, "vis_full_frame": False}
-)
-world.set_seed(47)
-
-def test(name, model, world, car, display, camera, device):
+def test(name, model, world, car, display, camera, device, f):
     print(f"Testing {name}...")
     world.set_seed(47)
     vista_reset(world, display)
@@ -71,13 +36,11 @@ def test(name, model, world, car, display, camera, device):
         observation = grab_and_preprocess_obs(car, camera).to(device)
         done = False
         prev_curvature = 0.0
-        
         trace_index = car.trace_index
         initial_frame = car.frame_index
         total_frames = len(car.trace.good_frames['camera_front'][0])
         track_left = total_frames - initial_frame
         episode_reward = 0.0
-            
         while not done:
             if name == "A2C":
                 mu, sigma = model.pi(observation.permute(2,0,1))
@@ -109,33 +72,69 @@ def test(name, model, world, car, display, camera, device):
     print(f"Model: {name}, avg score : {score/num_test:.1f}\n")
     return score/num_test # return avg score
 
-# Evaluation Hyperparameters
-max_curvature=1/8.0
-max_std=0.1
-models = [("A2C", a2c_model), ("REINFORCE", reinforce_model)]
+def main(args):
+    # REINFORCE
+    reinforce_model = mycnn.CNN().to(device)
+    reinforce_cnn_dict = torch.load('saved_models/REINFORCE_CNN_model.pth')
+    reinforce_model.load_state_dict(reinforce_cnn_dict['model_state_dict'])
+    reinforce_model.eval()
 
-now = datetime.datetime.now()
-timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+    # A2C
+    a2c_model = a2c_cnn.ActorCritic().to(device)
+    a2c_cnn_dict = torch.load('saved_models/best_a2c_model_checkpoint.pth')
+    a2c_model.load_state_dict(a2c_cnn_dict['model_state_dict'])
+    a2c_model.eval()
 
-results_dir = "eval_results"
-model_results_dir = results_dir
+    # TODO: PPO
 
-# frames_dir = "frames"
-# model_frame_dir = (frames_dir + f"/{model_name}_frames_{timestamp}/")
+    # Start VISTA Environment
+    trace_root = "vista_traces"
+    trace_path = [
+        "20210726-154641_lexus_devens_center",
+        "20210726-155941_lexus_devens_center_reverse",
+        "20210726-184624_lexus_devens_center",
+        "20210726-184956_lexus_devens_center_reverse",
+    ]
+    trace_path = [os.path.join(trace_root, p) for p in trace_path]
+    world = vista.World(trace_path, trace_config={"road_width": 4})
+    car = world.spawn_agent(config={"length": 5.0,
+                                    "width": 2.0,
+                                    "wheel_base": 2.78,
+                                    "steering_ratio": 14.7,
+                                    "lookahead_road": True,})
+    camera = car.spawn_camera(config={"size": (355, 413)})
+    display = vista.Display(
+        world, display_config={"gui_scale": 2, "vis_full_frame": False}
+    )
+    world.set_seed(47)
 
-# if self.animate and not os.path.exists(self.model_frame_dir):
-#     os.makedirs(self.model_frame_dir)
+    # Evaluation Hyperparameters
+    models = [("A2C", a2c_model), ("REINFORCE", reinforce_model)]
 
-if not os.path.exists(model_results_dir):
-    os.makedirs(model_results_dir)
+    now = datetime.datetime.now()
+    timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
 
-# Define the file path
-filename = "reinforce_v_a2c"
-print("Writing log to: " + filename + ".txt")
-file_path = os.path.join(model_results_dir, filename + ".txt")
-f = open(file_path, "w")
-f.write("model\treward\tsteps\ttrace\tdone\tcompleted\n")
+    results_dir = "eval_results"
+    model_results_dir = results_dir
 
-for name, model in models:
-    test(name, model, world, car, display, camera, device)
+    # if self.animate and not os.path.exists(self.model_frame_dir):
+    #     os.makedirs(self.model_frame_dir)
+
+    if not os.path.exists(model_results_dir):
+        os.makedirs(model_results_dir)
+
+    # Define the file path
+    filename = args.filename
+    print("Writing log to: " + filename + ".txt")
+    file_path = os.path.join(model_results_dir, filename + ".txt")
+    f = open(file_path, "w")
+    f.write("model\treward\tsteps\ttrace\tdone\tcompleted\n")
+
+    for name, model in models:
+        test(name, model, world, car, display, camera, device, f)
     
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description="VISTA DRL Evaluator")
+    parser.add_argument("-f", "--filename", required=True)
+    args = parser.parse_args()
+    main(args)
