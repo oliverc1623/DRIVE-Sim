3994d22976116ec13994ada0417510c351c00aa8
oliverc1623 <oliverc1622@gmail.com>
2023-11-01 19:02:35 +0000

    added sumtree and started PER buffer

---
+++ vista/PrioritizedExperienceReplayBuffer.py
@@ -0,0 +1,150 @@
+import copy
+import warnings
+from typing import Any, Dict, List, Optional, Union
+
+import numpy as np
+import random
+import torch as th
+from SumTree import SumTree
+from gymnasium import spaces
+
+from stable_baselines3.common.buffers import ReplayBuffer
+from stable_baselines3.common.type_aliases import DictReplayBufferSamples
+from stable_baselines3.common.vec_env import VecEnv, VecNormalize
+
+class PrioritizedExperienceReplayBuffer(ReplayBuffer):
+    env: Optional[VecEnv]
+
+    def __init__(
+        self,
+        buffer_size: int,
+        observation_space: spaces.Space,
+        action_space: spaces.Space,
+        alpha: float,
+        device: Union[th.device, str] = "auto",
+        n_envs: int = 1,
+        optimize_memory_usage: bool = False,
+        handle_timeout_termination: bool = True,
+    ):
+        super().__init__(buffer_size, observation_space, action_space, device, n_envs=n_envs)
+        
+        # Adjust buffer size
+        self.buffer_size = max(buffer_size // n_envs, 1)
+
+        # Check that the replay buffer can fit into the memory
+        if psutil is not None:
+            mem_available = psutil.virtual_memory().available
+
+        # there is a bug if both optimize_memory_usage and handle_timeout_termination are true
+        # see https://github.com/DLR-RM/stable-baselines3/issues/934
+        if optimize_memory_usage and handle_timeout_termination:
+            raise ValueError(
+                "ReplayBuffer does not support optimize_memory_usage = True "
+                "and handle_timeout_termination = True simultaneously."
+            )
+        self.optimize_memory_usage = optimize_memory_usage
+
+        self.observations = np.zeros((self.buffer_size, self.n_envs, *self.obs_shape), dtype=observation_space.dtype)
+
+        if not optimize_memory_usage:
+            # When optimizing memory, `observations` contains also the next observation
+            self.next_observations = np.zeros((self.buffer_size, self.n_envs, *self.obs_shape), dtype=observation_space.dtype)
+
+        self.actions = np.zeros(
+            (self.buffer_size, self.n_envs, self.action_dim), dtype=self._maybe_cast_dtype(action_space.dtype)
+        )
+
+        self.rewards = np.zeros((self.buffer_size, self.n_envs), dtype=np.float32)
+        self.dones = np.zeros((self.buffer_size, self.n_envs), dtype=np.float32)
+
+        self.alpha = alpha
+        self.priority_sum = [0 for _ in range(2 * self.buffer_size)]
+        self.priority_min = [float('inf') for _ in range(2 * self.buffer_size)]
+        self.max_priority = 1.
+        self.next_idx = 0
+        self.size = 0
+
+    def add(
+        self,
+        obs: np.ndarray,
+        next_obs: np.ndarray,
+        action: np.ndarray,
+        reward: np.ndarray,
+        done: np.ndarray,
+        infos: List[Dict[str, Any]],
+    ) -> None:
+        # Reshape needed when using multiple envs with discrete observations
+        # as numpy cannot broadcast (n_discrete,) to (n_discrete, 1)
+        if isinstance(self.observation_space, spaces.Discrete):
+            obs = obs.reshape((self.n_envs, *self.obs_shape))
+            next_obs = next_obs.reshape((self.n_envs, *self.obs_shape))
+
+        # Reshape to handle multi-dim and discrete action spaces, see GH #970 #1392
+        action = action.reshape((self.n_envs, self.action_dim))
+
+        # Copy to avoid modification by reference
+        self.observations[self.pos] = np.array(obs)
+
+        if self.optimize_memory_usage:
+            self.observations[(self.pos + 1) % self.buffer_size] = np.array(next_obs)
+        else:
+            self.next_observations[self.pos] = np.array(next_obs)
+
+        self.actions[self.pos] = np.array(action)
+        self.rewards[self.pos] = np.array(reward)
+        self.dones[self.pos] = np.array(done)
+
+        if self.handle_timeout_termination:
+            self.timeouts[self.pos] = np.array([info.get("TimeLimit.truncated", False) for info in infos])
+
+        self.pos += 1
+        if self.pos == self.buffer_size:
+            self.full = True
+            self.pos = 0
+        self.size = min(self.buffer_size, self.size + 1)
+
+        priority_alpha = self.max_priority ** self.alpha
+        self._set_priority_min(self.pos, priority_alpha)
+        self._set_priority_sum(self.pos, priority_alpha)
+
+    def _set_priority_min(self, idx, priority_alpha):
+        idx += self.buffer_size
+        self.priority_min[idx] = priority_alpha
+        while idx >= 2:
+            # Get the index of the parent node
+            idx //= 2
+            # Value of the parent node is the minimum of it's two children
+            self.priority_min[idx] = min(self.priority_min[2 * idx], self.priority_min[2 * idx + 1])
+
+    def _set_priority_sum(self, idx, priority):
+        idx += self.buffer_size
+        self.priority_min[idx] = priority_alpha
+        while idx >= 2:
+            # Get the index of the parent node
+            idx //= 2
+            # Value of the parent node is the minimum of it's two children
+            self.priority_min[idx] = min(self.priority_min[2 * idx], self.priority_min[2 * idx + 1])
+
+    def _sum(self):
+        return self.priority_sum[1]
+
+    def _min(self):
+        return self.priority_min[1]
+
+    def find_prefix_sum_id(self, prefix_sum):
+        idx = 1
+        while idx < self.buffer_size:
+            if self.priority_sum[idx * 2] > prefix_sum:
+                idx = 2 * idx
+            else:
+                prefix_sum -= self.priority_sum[idx * 2]
+                idx = 2 * idx + 1
+        return idx - self.capacity
+
+    
+    def sample(self, batch_size, beta):
+        # (self.buffer_size, self.n_envs)
+        samples = {
+            'weights': np.zeros(shape=(batch_size, self.n_envs), dtype=np.float32),
+            'indexes': np.zeros(shape=(batch_size, self.n_envs), dtype=np.int32)
+        }
