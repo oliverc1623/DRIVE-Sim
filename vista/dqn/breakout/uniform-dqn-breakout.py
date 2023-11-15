3459530f0d91c5c0d9a16375e0b50bcbf302f6e4
oliverc1623 <oliverc1622@gmail.com>
2023-11-15 06:19:09 +0000

    buffer saves numpy arrays and fixed stacking dims

--- vista/dqn/breakout/uniform-dqn-breakout.py
+++ vista/dqn/breakout/uniform-dqn-breakout.py
@@ -15,52 +15,44 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 #Hyperparameters
-learning_rate = 1e-3
+learning_rate = 1e-4
 gamma         = 0.99
-buffer_limit  = 50_000
+buffer_limit  = 500_000
 batch_size    = 32
 
-class ReplayBuffer:
-    def __init__(self, state_size, state_size2, action_size, buffer_size, device):
-        # state, action, reward, next_state, done
-        self.state = torch.empty(buffer_size, 1, state_size, state_size2, dtype=torch.float)
-        self.action = torch.empty(buffer_size, action_size, dtype=torch.float)
-        self.reward = torch.empty(buffer_size, dtype=torch.float)
-        self.next_state = torch.empty(buffer_size, 1, state_size, state_size2, dtype=torch.float)
-        self.done = torch.empty(buffer_size, dtype=torch.int)
+if torch.cuda.is_available():
+    device= 'cuda:0'
+else:
+    device = 'cpu'
+print(f'device:{device}')
 
-        self.count = 0
-        self.real_size = 0
-        self.size = buffer_size
+class ReplayBuffer():
+    def __init__(self, device):
+        self.buffer = collections.deque(maxlen=buffer_limit)
         self.device = device
+    
+    def put(self, transition):
+        self.buffer.append(transition)
+    
+    def sample(self, n):
+        mini_batch = random.sample(self.buffer, n)
+        s_lst, a_lst, r_lst, s_prime_lst, done_mask_lst = [], [], [], [], []
+        
+        for transition in mini_batch:
+            s, a, r, s_prime, done_mask = transition
+            s_lst.append(s)
+            a_lst.append([a])
+            r_lst.append([r])
+            s_prime_lst.append(s_prime)
+            done_mask_lst.append([done_mask])
 
-    def add(self, transition):
-        state, action, reward, next_state, done = transition
+        return (torch.tensor(np.array(s_lst), dtype=torch.float).permute(0,3,1,2).to(self.device))/ 255., torch.tensor(a_lst).to(self.device), \
+               torch.tensor(np.array(r_lst)).to(self.device), \
+               (torch.tensor(np.array(s_prime_lst), dtype=torch.float).permute(0,3,1,2).to(self.device))/ 255., \
+               torch.tensor(np.array(done_mask_lst)).to(self.device)
 
-        # store transition in the buffer
-        self.state[self.count] = torch.as_tensor(state).unsqueeze(0)
-        self.action[self.count] = torch.as_tensor(action)
-        self.reward[self.count] = torch.as_tensor(reward)
-        self.next_state[self.count] = torch.as_tensor(next_state).unsqueeze(0)
-        self.done[self.count] = torch.as_tensor(done)
-
-        # update counters
-        self.count = (self.count + 1) % self.size
-        self.real_size = min(self.size, self.real_size + 1)
-
-    def sample(self, batch_size):
-        assert self.real_size >= batch_size
-
-        sample_idxs = np.random.choice(self.real_size, batch_size, replace=False)
-
-        batch = (
-            self.state[sample_idxs].to(self.device),
-            self.action[sample_idxs].to(self.device),
-            self.reward[sample_idxs].to(self.device),
-            self.next_state[sample_idxs].to(self.device),
-            self.done[sample_idxs].to(self.device)
-        )
-        return batch
+    def size(self):
+        return len(self.buffer)
 
 class Qnet(nn.Module):
     def __init__(self, n_input_channels, n_actions):
@@ -68,21 +60,21 @@ class Qnet(nn.Module):
         self.conv1 = nn.Conv2d(n_input_channels, 32, kernel_size=8, stride=4, padding=0)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0)
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0)
-        # 22 and 16 come from
-        # 22 =~ (((((210 - 8 + 1)/4) - 4 + 1) / 2) - 3 + 1) / 1 
-        # 16 =~ (((((160 - 8 + 1)/4) - 4 + 1) / 2) - 3 + 1) / 1
-        self.mlp = nn.Linear(17024, n_actions)
+        self.mlp1 = nn.Linear(3136, 256)
+        self.mlp2 = nn.Linear(256, n_actions)
 
     def forward(self, x):
-        x = F.silu(self.conv1(x))
-        x = F.silu(self.conv2(x))
-        x = F.silu(self.conv3(x))
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
         # Flatten before fully connected layers
-        x = x.view(x.size(0), -1)
-        x = self.mlp(x)    
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = self.mlp1(x)    
+        x = self.mlp2(x)
         return x
 
     def sample_action(self, obs, epsilon):
+        obs = (torch.tensor(obs).permute(2,0,1) / 255.).unsqueeze(0).to(device)
         out = self.forward(obs)
         coin = random.random()
         if coin < epsilon:
@@ -94,11 +86,11 @@ def train(q, q_target, memory, optimizer):
     s,a,r,s_prime,done_mask = memory.sample(batch_size)
 
     # Q(s′,argmax a ′Q(s ′,a ′;θ i);θ i−)
-    Q_next = q_target(s_prime).max(1)[0]
+    Q_next = q_target(s_prime).max(1)[0].unsqueeze(1)
     y_i = r + done_mask * gamma * Q_next
 
     # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
-    Q = q(s)[torch.arange(len(a)), a.to(torch.long).flatten()]
+    Q = q(s).gather(1,a)
 
     assert Q.shape == y_i.shape, f"{Q.shape}, {y_i.shape}"
 
@@ -110,21 +102,17 @@ def train(q, q_target, memory, optimizer):
 
 # Convert image to greyscale, resize and normalise pixels
 def preprocess(image, width, height, targetWidth, targetHeight):
-	image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	image = image[20:300, 0:200]  # crop off score
-	image = cv2.resize(image, (targetWidth, targetHeight))
-	image = image.reshape(targetWidth, targetHeight)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = image[20:300, 0:200]  # crop off score
+    image = cv2.resize(image, (targetWidth, targetHeight))
+    image = image.reshape(targetWidth, targetHeight)
+    image = np.expand_dims(image, axis=2)
     
-	return image
+    return image
 
 def main():
     env = gym.make('BreakoutNoFrameskip-v4')
-    if torch.cuda.is_available():
-        device= 'cuda:0'
-    else:
-        device = 'cpu'
-    print(f'device:{device}')
-        
+
     # set seed for reproducibility
     seed = int(sys.argv[1])
     os.environ["PYTHONHASHSEED"] = str(seed)
@@ -133,79 +121,71 @@ def main():
     random.seed(seed)
     torch.manual_seed(seed)
 
-    q = Qnet(1,4)
-    q_target = Qnet(1,4)
+    q = Qnet(4,4)
+    q_target = Qnet(4,4)
     q_target.load_state_dict(q.state_dict())
     q.to(device)
     q_target.to(device)
-    memory = ReplayBuffer(84, 336, 1, buffer_limit, device)
+    memory = ReplayBuffer(device)
 
     print_interval = 1
-    target_update_interval = 4
     train_update_interval = 4
-    train_start = 1000
-    score = 0.0
+    target_update_interval = 1_000
+    train_start = 100_000
+    score = 0
+    step = 0
     optimizer = optim.Adam(q.parameters(), lr=learning_rate)
 
     with open(f'../data/breakout/Uniform-DQN-Breakout{sys.argv[1]}.csv', 'w', newline='') as csvfile:
         fieldnames = ['step', 'score']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-
-        for n_epi in range(500):
+        
+        for n_epi in range(5000):
             epsilon = max(0.01, 0.1 - 0.01*(n_epi/200)) #Linear annealing from 10% to 1%
             observation, info = env.reset()
 
             # preprocess
             observation = preprocess(observation, 210, 160, 84, 84)
-            observation = np.expand_dims(observation, axis=2)
 
+            # stack images
             img1 = np.zeros((84,84,1)).astype(np.uint8)
             img2 = np.zeros((84,84,1)).astype(np.uint8)
             img3 = np.zeros((84,84,1)).astype(np.uint8)
             history = deque([img1, img2, img3, observation])
-            stacked_images = np.concatenate(list(history), axis=1)
-
-            observation = (torch.tensor(stacked_images).permute(2,0,1) / 255.).unsqueeze(0).to(device)
+            observation = np.concatenate(list(history), axis=-1)
 
             terminated = False
             truncated = False
-            step = 1
             while not terminated and not truncated:
                 action = q.sample_action(observation, epsilon)      
                 observation_prime, reward, terminated, truncated, info = env.step(action)
                 done_mask = 0.0 if terminated else 1.0
                 
                 observation_prime = preprocess(observation_prime, 210, 160, 84, 84)
-                observation_prime = np.expand_dims(observation_prime, axis=2)
 
                 history.popleft()
                 history.append(observation_prime)
 
-                stacked_images = np.concatenate(list(history), axis=1)
-
-                # img = Image.fromarray(stacked_images[:,:,0])
-                # img.save(f"file{step:03}.png")
-                
-                observation_prime = (torch.tensor(stacked_images).permute(2,0,1) / 255.).unsqueeze(0).to(device)
-                memory.add((observation,action,reward,observation_prime, done_mask))
+                observation_prime = np.concatenate(list(history), axis=-1)
+                memory.put((observation,action,reward,observation_prime, done_mask))
                 
                 observation = observation_prime
+
+                if step>train_start and step%train_update_interval==0:
+                    train(q, q_target, memory, optimizer)
+    
+                if step>train_start and step%target_update_interval==0:
+                    q_target.load_state_dict(q.state_dict())
                     
                 score += reward
                 step += 1
                 if terminated:
                     break
-    
-            if memory.real_size>train_start and n_epi % train_update_interval == 0:
-                train(q, q_target, memory, optimizer)
 
-            if n_epi%target_update_interval==0 and n_epi!=0:
-                q_target.load_state_dict(q.state_dict())
-                
-            if n_epi%print_interval==0: #and n_epi!=0:
-                print(f"step :{n_epi}, score : {score/print_interval:.1f}, n_buffer : {memory.real_size}, eps : {epsilon*100:.1f}%")
-                writer.writerow({"step": n_epi, "score":score/print_interval})
+            if n_epi%print_interval==0:
+                print(f"episode :{n_epi}, step: {step}, score : {score/print_interval:.1f}, n_buffer : {memory.size()}, eps : {epsilon*100:.1f}%")
+                writer.writerow({"step": step, "score":score/print_interval})
                 csvfile.flush()
                 score = 0.0
 
