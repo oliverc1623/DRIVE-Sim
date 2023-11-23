734ca79b5599121d906c90f4e6c6d320275dc35b
Oliver Chang <oliverc1622@gmail.com>
2023-11-23 15:15:23 -0700

    can render rgb images of game

--- vista/dqn/minigrid/m2-dqn-minigrid.py
+++ vista/dqn/minigrid/m2-dqn-minigrid.py
@@ -1,6 +1,6 @@
 import gymnasium as gym
 import minigrid
-from minigrid.wrappers import ImgObsWrapper,RGBImgPartialObsWrapper,RGBImgObsWrapper
+from minigrid.wrappers import ImgObsWrapper, RGBImgPartialObsWrapper, RGBImgObsWrapper
 import collections
 import random
 import numpy as np
@@ -8,38 +8,41 @@ import sys, os
 import csv
 import cv2
 import matplotlib.pyplot as plt
+from IPython import display
 from PIL import Image
 from collections import deque
+import time
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-#Hyperparameters
+# Hyperparameters
 learning_rate = 1e-4
-gamma         = 0.99
-buffer_limit  = 50_000
-batch_size    = 32
+gamma = 0.99
+buffer_limit = 50_000
+batch_size = 32
 
 if torch.backends.mps.is_available():
-    device= 'mps'
+    device = "mps"
 else:
-    device = 'cpu'
-print(f'device:{device}')
+    device = "cpu"
+print(f"device:{device}")
 
-class ReplayBuffer():
+
+class ReplayBuffer:
     def __init__(self, device):
         self.buffer = collections.deque(maxlen=buffer_limit)
         self.device = device
-    
+
     def put(self, transition):
         self.buffer.append(transition)
-    
+
     def sample(self, n):
         mini_batch = random.sample(self.buffer, n)
         s_lst, a_lst, r_lst, s_prime_lst, done_mask_lst = [], [], [], [], []
-        
+
         for transition in mini_batch:
             s, a, r, s_prime, done_mask = transition
             s_lst.append(s)
@@ -48,13 +51,23 @@ class ReplayBuffer():
             s_prime_lst.append(s_prime)
             done_mask_lst.append([done_mask])
 
-        return (torch.tensor(np.array(s_lst), dtype=torch.float32).permute(0,3,1,2).to(self.device))/ 255., torch.tensor(a_lst,dtype=torch.float32).to(self.device), \
-               torch.tensor(np.array(r_lst), dtype=torch.float32).to(self.device), \
-               (torch.tensor(np.array(s_prime_lst), dtype=torch.float32).permute(0,3,1,2).to(self.device))/ 255., \
-               torch.tensor(np.array(done_mask_lst), dtype=torch.float32).to(self.device)
+        return (
+            torch.tensor(np.array(s_lst), dtype=torch.int)
+            .permute(0, 3, 1, 2)
+            .to(self.device)
+            / 255.0,
+            torch.tensor(a_lst, dtype=torch.int).to(self.device),
+            torch.tensor(np.array(r_lst), dtype=torch.int).to(self.device),
+            torch.tensor(np.array(s_prime_lst), dtype=torch.int)
+            .permute(0, 3, 1, 2)
+            .to(self.device)
+            / 255.0,
+            torch.tensor(np.array(done_mask_lst), dtype=torch.int).to(self.device),
+        )
 
     def size(self):
         return len(self.buffer)
+
 
 class Qnet(nn.Module):
     def __init__(self, n_input_channels, n_actions):
@@ -66,25 +79,27 @@ class Qnet(nn.Module):
         self.mlp2 = nn.Linear(512, n_actions)
 
     def forward(self, x):
+        x = x.to(torch.float32)
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
         x = F.relu(self.mlp1(x))
         x = self.mlp2(x)
         return x
 
     def sample_action(self, obs, epsilon):
-        obs = (torch.tensor(obs).permute(2,0,1) / 255.).unsqueeze(0).to(device)
+        obs = (torch.tensor(obs).permute(2, 0, 1) / 255.0).unsqueeze(0).to(device)
         out = self.forward(obs)
         coin = random.random()
         if coin < epsilon:
-            return random.randint(0,2)
-        else : 
+            return random.randint(0, 2)
+        else:
             return out.argmax().item()
-            
+
+
 def train(q, q_target, memory, optimizer):
-    s,a,r,s_prime,done_mask = memory.sample(batch_size)
+    s, a, r, s_prime, done_mask = memory.sample(batch_size)
 
     # Rt+1 + γ max_a Q(S_t+1, a; θt). where θ=θ- because we update target params to train params every t steps
     Q_next = q_target(s_prime).max(1)[0].unsqueeze(1)
@@ -92,7 +107,7 @@ def train(q, q_target, memory, optimizer):
     y_i = r + done_mask * gamma * Q_next
 
     # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
-    Q = q(s)[torch.arange(len(a)), a.to(torch.long).flatten()]  #q(s).gather(1,a)
+    Q = q(s)[torch.arange(len(a)), a.to(torch.long).flatten()]  # q(s).gather(1,a)
     # print(f"Q-value estimate: {q(s)}\n")
     # print(f"Q_next: {Q_next}")
     # print(f"target: {y_i}")
@@ -108,15 +123,17 @@ def train(q, q_target, memory, optimizer):
 
     return Q.mean()
 
+
 # Convert image to greyscale, resize and normalise pixels
 def preprocess(image):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     image = np.expand_dims(image, axis=2)
     return image
 
+
 def main():
     env = gym.make("MiniGrid-Empty-5x5-v0", render_mode="rgb_array")
-    env = RGBImgObsWrapper(env) # Get pixel observations
+    env = RGBImgObsWrapper(env)  # Get pixel observations
 
     # set seed for reproducibility
     seed = int(sys.argv[1])
@@ -126,8 +143,8 @@ def main():
     random.seed(seed)
     torch.manual_seed(seed)
 
-    q = Qnet(1,3)
-    q_target = Qnet(1,3)
+    q = Qnet(1, 3)
+    q_target = Qnet(1, 3)
     q_target.load_state_dict(q.state_dict())
     q.to(device)
     q_target.to(device)
@@ -143,53 +160,74 @@ def main():
     optimizer = optim.Adam(q.parameters(), lr=learning_rate)
     q_value = torch.tensor(0)
 
-    with open(f'../data/minigrid/Uniform-DQN-Minigrid{sys.argv[1]}.csv', 'w', newline='') as csvfile:
-        fieldnames = ['step', 'score', 'Q-value']
+    with open(
+        f"../data/minigrid/Uniform-DQN-Minigrid{sys.argv[1]}.csv", "w", newline=""
+    ) as csvfile:
+        fieldnames = ["step", "score", "Q-value"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-        
+
+        fg = plt.figure()
+        ax = fg.gca()
+
         for n_epi in range(500):
             observation, info = env.reset()
 
             # preprocess
-            observation = preprocess(observation['image'])
+            observation = preprocess(observation["image"])
+            h = ax.imshow(observation)
 
             terminated = False
             truncated = False
             while not terminated and not truncated:
-                epsilon = max(0.1, 1.0 - 0.01*(step/total_frames)) #Linear annealing from 1.0 to 0.1
-                action = q.sample_action(observation, epsilon)      
-                observation_prime, reward, terminated, truncated, info = env.step(action)
+                epsilon = max(
+                    0.1, 1.0 - 0.01 * (step / total_frames)
+                )  # Linear annealing from 1.0 to 0.1
+                action = q.sample_action(observation, epsilon)
+                observation_prime, reward, terminated, truncated, info = env.step(
+                    action
+                )
 
+                h.set_data(observation_prime["image"])
+                plt.draw(), plt.pause(1e-3)
                 # uncomment block to download frames as PNGs
                 # img = Image.fromarray((observation_prime['image']).astype(np.uint8))
                 # img = img.resize((128,128), resample=Image.BOX)
                 # img.save(f"frames/frame_{step:04d}.png")
-                
+
                 done_mask = 0.0 if terminated else 1.0
-                observation_prime = preprocess(observation_prime['image'])
-                memory.put((observation,action,reward,observation_prime, done_mask))
-                
+                observation_prime = preprocess(observation_prime["image"])
+                memory.put((observation, action, reward, observation_prime, done_mask))
+
                 observation = observation_prime
 
-                if step>train_start and step%train_update_interval==0:
+                if step > train_start and step % train_update_interval == 0:
                     q_value = train(q, q_target, memory, optimizer)
-    
-                if step>train_start and step%target_update_interval==0:
+
+                if step > train_start and step % target_update_interval == 0:
                     q_target.load_state_dict(q.state_dict())
-                    
+
                 score += reward
                 step += 1
                 if terminated:
                     break
 
-            if n_epi%print_interval==0:
-                print(f"episode :{n_epi}, step: {step}, score : {score/print_interval:.1f}, n_buffer : {memory.size()}, eps : {epsilon*100:.1f}%")
-                writer.writerow({"step": step, "score":score/print_interval, "Q-value":q_value.item()})
+            if n_epi % print_interval == 0:
+                print(
+                    f"episode :{n_epi}, step: {step}, score : {score/print_interval:.1f}, n_buffer : {memory.size()}, eps : {epsilon*100:.1f}%"
+                )
+                writer.writerow(
+                    {
+                        "step": step,
+                        "score": score / print_interval,
+                        "Q-value": q_value.item(),
+                    }
+                )
                 csvfile.flush()
                 score = 0.0
 
     env.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
