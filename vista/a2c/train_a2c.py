0fb281a95d776f7b12f33d97ff2adc14ef2468be
olverc1623 <oliverc1622@gmail.com>
2024-05-24 02:09:51 +0000

    a2c updated

--- vista/a2c/train_a2c.py
+++ vista/a2c/train_a2c.py
@@ -10,6 +10,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath('CustomCNN.py'))
 from CustomCNN import CustomCNN
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath('SeqTransformer.py'))))
 from SeqTransformer import SeqTransformer
+from typing import Callable
 
 # Standard Torch
 import copy
@@ -28,8 +29,7 @@ from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.utils import set_random_seed
 from stable_baselines3.common.callbacks import StopTrainingOnMaxEpisodes
 
-
-device = ("cuda:0" if torch.cuda.is_available() else "cpu")
+device = ("cuda:1" if torch.cuda.is_available() else "cpu")
 device = torch.device(device)
 print(f"Using {device} device")
 
@@ -83,43 +83,56 @@ def make_env(rank: int, seed: int = 47):
                display_config = display_config,
                preprocess_config = preprocess_config,
                sensors_configs = [camera_config])
-        env.reset(seed=rank+seed)
+        env.set_seed(seed + rank)
+        env.reset()
         return env
-    set_random_seed(seed)
+    set_random_seed(seed + rank)
     time.sleep(1)
     return _init
 
+def linear_schedule(initial_value: float) -> Callable[[float], float]:
+    def func(progress_remaining: float) -> float:
+        return p
+
 learning_configs = {
-    "policy_type": "CustomCnnPolicy",
-    "total_timesteps": 100_000,
+    "policy_type": CustomCNN,
+    "total_timesteps": 500_000,
     "env_id": "VISTA",
-    "learning_rate": 0.0003
+    "learning_rate": 0.0001, #linear_schedule(0.0001),
+    "n_steps": 2048,
+    "batch_size": 32,
+    "ent_coef": 0.0,
 }
 
 if __name__ == "__main__":
-    callback_max_episodes = StopTrainingOnMaxEpisodes(max_episodes=100, verbose=1)
-    for i in range(1,5):
-        torch.cuda.empty_cache()
-        num_cpu = 8 # Number of processes to use
-        vec_env = SubprocVecEnv([make_env(i) for i in range(num_cpu)])
-        # Frame-stacking with 4 frames
-        vec_env = VecFrameStack(vec_env, n_stack=4)
-        # Create log dir
-        log_dir = f"a2c_stacked_gray{i}/"
-        os.makedirs(log_dir, exist_ok=True)
-        vec_env = VecMonitor(vec_env, log_dir, ('out_of_lane', 'exceed_max_rot', 'distance', 'agent_done'))
-        model = A2C(
-            "CnnPolicy", 
-            vec_env,
-            learning_rate = learning_configs['learning_rate'],
-            verbose=1,
-            device=device
-        )
-        timesteps = learning_configs['total_timesteps']
-        model.learn(
-            total_timesteps=timesteps, 
-            callback=callback_max_episodes,
-            progress_bar=True
-        )
-        # Save the agent
-        model.save(f"a2c_stacked_gray_{i}")
+    callback_max_episodes = StopTrainingOnMaxEpisodes(max_episodes=25, verbose=1)
+    torch.cuda.empty_cache()
+    num_cpu = 8 
+    vec_env = SubprocVecEnv([make_env(i) for i in range(num_cpu)])
+    vec_env = VecFrameStack(vec_env, n_stack=4)
+    
+    # Create log dir
+    log_dir = f"/mnt/persistent/lane-follow-a2c/tmp_{sys.argv[1]}/"
+    os.makedirs(log_dir, exist_ok=True)
+    vec_env = VecMonitor(vec_env, log_dir, ('out_of_lane', 'exceed_max_rot', 'agent_done', 'course_completion_rate'))
+    policy_kwargs = dict(
+        features_extractor_class=learning_configs['policy_type'],
+        features_extractor_kwargs=dict(features_dim=128),
+    )
+
+    model = A2C(
+        "CnnPolicy", 
+        vec_env,
+        learning_rate=learning_configs['learning_rate'],
+        n_steps=learning_configs['n_steps'],
+        verbose=1,
+        device=device
+    )
+    timesteps = learning_configs['total_timesteps']
+    model.learn(
+        total_timesteps=timesteps, 
+        callback=callback_max_episodes,
+        progress_bar=True
+    )
+    # Save the agent
+    model.save(f"/mnt/persistent/lane-follow-a2c/tmp_{sys.argv[1]}/a2c-model-trial{sys.argv[1]}_customCNN")
